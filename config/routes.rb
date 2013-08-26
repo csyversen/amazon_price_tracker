0985b90c72b0ce6d3eb4ef810bdbb966f50c4344
Chris <csyversen@gmail.com>
2013-08-25 19:45:39 -0500

    adding gems for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ AmazonPriceTracker::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'track#menu'
+  root 'user/'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
